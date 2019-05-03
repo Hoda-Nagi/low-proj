f09de8fdeb3dbc81e013f367475ff4afefb9b5ed
dodonagi <48260197+dodonagi@users.noreply.github.com>
2019-05-03 02:25:22 +0200

    Add files via upload

---
+++ js/plugin.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+// $(".our-team img").mouseenter(function() {
+// 	$(".our-team .overlay").css("display","block");
+// 	$(".our-team p.name").css("marginTop","-286px");
+// 	$(".our-team hr").css("borderTop", "1px solid #222");
+// });
+// $(".our-team img").mouseleave(function() {
+// 	$(".our-team .overlay").css("display","none");
+// 	$(".our-team p.name").css("marginTop"," 13px;");
+// 	$(".our-team hr").css("borderTop", "1px solid rgba(0,0,0,.1)")
+// });
+
+
+
+
+
+
+
+});
